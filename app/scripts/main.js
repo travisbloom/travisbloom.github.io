cd23c76ed5abad439446d61fb9aaac856ff5e706
travisbloom <travisbloom@gmail.com>
2014-12-20 17:51:41 -0500

    using gulp to build and deploy, refactored majority of css, reduced complexity

---
+++ app/scripts/main.js
@@ -0,0 +1,41 @@
+$(function() {
+  var openSection;
+  //set the header text to the navIcon's section
+  $( ".category-header" ).click(function() {
+    openSection = $(this).attr('name');
+  });
+//    //moves down the page the first time a section is clicked
+//    $( ".navIcon" ).one( "click", function() {
+//        var n = $(document).height();
+//        $('html, body').animate(
+//            { scrollTop: 500 }, 500);
+//    });
+  //attaches the fastClick plugin to the body, improving mobile responsiveness
+  FastClick.attach(document.body);
+  //toggles the animation class for the sidebar
+  $("#aside-toggle").click(function() {
+    $("header").toggleClass("show-aside");
+    $("main").toggleClass("show-aside");
+  });
+  //Header Scroll Effect
+  $('#iconBox').waypoint(function(direction) {
+    $("#headerText").text(openSection);
+    if (direction === "up"){
+      $("#headerText").text("");
+      $("#header-holder").removeClass().addClass("clean");
+    }
+    else{
+      $("#header-holder").removeClass().addClass("headerScroll");
+    }
+  });
+
+
+  Handlebars.registerHelper('setParentIndex', function(value) {
+    this.index = Number(value); //I needed human readable index, not zero based
+  });
+  //compile handlebars template
+  var template = Handlebars.compile($("#stories").html());
+  //inject each panel group with the template
+  $('.panel-group').append(template(storyData));
+});
+

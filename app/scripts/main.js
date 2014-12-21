2c4a77389142efa9e7025805d11e3765bb5b6e62
travisbloom <travisbloom@gmail.com>
2014-12-21 14:47:08 -0500

    jquery edits, finalized css for stories

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,90 +1,29 @@
-<<<<<<< HEAD
 $(function() {
-  var openSection;
-  //set the header text to the navIcon's section
-  $( ".category-header" ).click(function() {
-    openSection = $(this).attr('name');
-  });
-//    //moves down the page the first time a section is clicked
-//    $( ".navIcon" ).one( "click", function() {
-//        var n = $(document).height();
-//        $('html, body').animate(
-//            { scrollTop: 500 }, 500);
-//    });
   //attaches the fastClick plugin to the body, improving mobile responsiveness
   FastClick.attach(document.body);
-  //toggles the animation class for the sidebar
-  $("#aside-toggle").click(function() {
-    $("header, main").toggleClass("show-aside");
+  //set the header text to the navIcon's section
+  $( '.category-header' ).click(function() {
+    //what section was clicked
+    var section = $(this).attr('name');
+    //capitalize the first letter and add it to the header section span
+    $('#section-text').html(section[0].toUpperCase() + section.slice(1));
   });
-//  //Header Scroll Effect
-//  $('.category-headers').waypoint(function(direction) {
-//    $("#headerText").text(openSection);
-//    if (direction === "up"){
-//      $("#headerText").text("");
-//      $("#header-holder").removeClass().addClass("clean");
-//    }
-//    else{
-//      $("#header-holder").removeClass().addClass("headerScroll");
-//    }
-//  });
-});
-
-=======
-//Header Scroll Effect
-$('#iconBox').waypoint(function(direction) {
-    $("#headerText").text(OpenSection);
-    if (direction === "up"){
-        $("#headerText").text("");
-        $("#headerHolder").removeClass().addClass("clean");
-    }
-    else{
-        $("#headerHolder").removeClass().addClass("headerScroll");
-    }
-
-});
-//sets the top text to the section name
-var OpenSection = "";
-$( ".navIcon" ).click(function() {
-    OpenSection = ($(this).attr('name'));
-});
-//moves down the page the first time a section is clicked
-$( ".navIcon" ).one( "click", function() {
-    var n = $(document).height();
-    $('html, body').animate({ scrollTop: 500 }, 500);
-});
-
-//toggles the animation class for the sidebar
-$("#headerClick").click(function() {
-    $(".page").toggleClass("nav-animation");
-});
-
-//prevents default click actions from occuring
-$('.stopJump').click(function(e) {
+  //toggles the animation class for the sidebar
+  $('#aside-toggle').click(function(e) {
     e.preventDefault();
+    $('header, main').toggleClass('show-aside');
+  });
+  //moves down the page the first time a section is clicked
+  $( '.category-header' ).one( 'click', function() {
+      $('html, body').animate(
+          { scrollTop: 550 }, 500
+      );
+  });
+ //show header items on scroll
+  $('#categories-container').waypoint({
+    handler: function(direction) {
+      console.log(direction);
+      $('header').toggleClass('scrolling');
+    }
+  });
 });
-
-//attaches the fastClick plugin to the body, improving mobile responsiveness
-$(function() {
-    FastClick.attach(document.body);
-});
-
-//adds the backstretch image to each section
-//for (var i = 0; i < storyData.length; i++) {
-//    console.log(storyData.length);
-//    console.log(storyData[i]);
-//    for (var x = 0; i < storyData[i].length; i++) {
-//        console.log(storyData[i].length);
-//        console.log('trig');
-//        $('#background' + x + i).backstretch('../images/sections/' + x + '_' + i + '.jpg');
-//    }
-//}
-
-Handlebars.registerHelper('setParentIndex', function(value){
-    this.index = Number(value); //I needed human readable index, not zero based
-});
-var source   = $("#stories").html();
-
-var template = Handlebars.compile(source);
-$('.panel-group').append(template(storyData));
->>>>>>> cd48ccec0c17218d33c063f9e318d945bc5cda72
